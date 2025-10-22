4e36a944be056bf48fca656ad622036f52f216f0
Sreerag40997 <srag40997@gmail.com>
2025-10-22 14:59:19 +0530

    update the token time

--- utils/token.go
+++ utils/token.go
@@ -16,7 +16,7 @@ func GenerateToken(userID uint, email string) (string, error) {
 	claims := jwt.MapClaims{
 		"user_id": userID,
 		"email":   email,
-		"exp":     time.Now().Add(time.Hour * 72).Unix(),
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
